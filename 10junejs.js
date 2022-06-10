232dc1febc917b2795b26087a986dce65ffa2767
Shubham Dixit <shubhamdixit863@gmail.com>
2022-06-10 22:09:06 +0530

    Create 10junejs.js

---
+++ 10junejs.js
@@ -0,0 +1,33 @@
+//Practice question
+const data=[
+  {
+    "_id": "62a372cbe5bb83afa31760a8",
+    "name": "Nikki Mosley",
+    "address": "782 Prince Street, Gallina, Utah, 4156"
+  },
+  {
+    "_id": "62a372cb48e56aa3d6016871",
+    "name": "Sampson Mercado",
+    "address": "148 Crystal Street, Macdona, South Dakota, 8488"
+  },
+  {
+    "_id": "62a372cb4aad1e8bc0525d17",
+    "name": "Johnston Myers",
+    "address": "846 Eldert Lane, Cashtown, North Dakota, 5586"
+  }
+  
+  
+]
+
+
+// output ["Nikki Mosley","Sampson Mercado","Johnston Myers"]
+
+
+
+
+
+
+
+
+
+
