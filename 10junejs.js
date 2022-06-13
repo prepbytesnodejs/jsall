9d5e4619c3acfb42d3f389e435dcc96e7da6e119
Shubham Dixit <shubhamdixit863@gmail.com>
2022-06-13 22:23:25 +0530

    Update 10junejs.js

--- 10junejs.js
+++ 10junejs.js
@@ -75,6 +75,51 @@ console.log(outputwithmap)
 
 
 
+//map function --->
+//1- if you want to apply an operation to every element of an array 
+// 2- you want output to be an array 
+// 3- you want the output array to have same length as input array 
+// 4- you want to do some operation on an array 
+
+
+// input array 
+
+const data=[
+  {
+    "_id": "62a372cbe5bb83afa31760a8",
+    "name": "Nikki Mosley",
+    "address": "782 Prince Street, Gallina, Utah, 4156"
+  },
+  {
+    "_id": "62a372cb48e56aa3d6016871",
+    "name": "Sampson Mercado",
+    "address": "148 Crystal Street, Macdona, South Dakota, 8488"
+  },
+  {
+    "_id": "62a372cb4aad1e8bc0525d17",
+    "name": "Johnston Myers",
+    "address": "846 Eldert Lane, Cashtown, North Dakota, 5586"
+  }
+  
+  
+]
+
+// output ["Nikki Mosley","Sampson Mercado","Johnston Myers"]
+// const temp= data.map(function foo(i,j){
+//   return i.name
+
+// })
+// console.log(temp)
+const inparray = ['AB' , 'AC', 'AD', 'BD'];
+var outarray = inparray.filter(function cb (enj){
+  if(enj.startsWith("A")){
+  return enj;
+    }
+})
+console.log(outarray)
+
+
+
 
 
 
